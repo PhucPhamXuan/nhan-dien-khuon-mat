71cbaa97d8b1620d2b41ae86fbb86520a55200f1
PhucPhamXuan <91009245+PhucPhamXuan@users.noreply.github.com>
2026-04-19 19:51:33 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ async function loadTrainingData() {
 	for (const label of labels) {
 		const descriptors = []
 		for (let i = 1; i <= 4; i++) {
-			const image = await faceapi.fetchImage(`/data/${label}/${i}.jpeg`)
+			const image = await faceapi.fetchImage(`/nhan-dien-khuon-mat/data/${label}/${i}.jpeg`)
 			const detection = await faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor()
 			descriptors.push(detection.descriptor)
 		}
@@ -25,9 +25,9 @@ async function loadTrainingData() {
 let faceMatcher
 async function init() {
 	await Promise.all([
-		faceapi.loadSsdMobilenetv1Model('/models'),
-		faceapi.loadFaceRecognitionModel('/models'),
-		faceapi.loadFaceLandmarkModel('/models'),
+		faceapi.loadSsdMobilenetv1Model('/nhan-dien-khuon-mat/models'),
+		faceapi.loadFaceRecognitionModel('/nhan-dien-khuon-mat/models'),
+		faceapi.loadFaceLandmarkModel('/nhan-dien-khuon-mat/models'),
 	])
 
 
